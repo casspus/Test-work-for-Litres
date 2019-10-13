73d54ad8648632ae96550351422e4434d3ee6bc9
KBelova <bcasspus@gmail.com>
2019-10-13 22:00:10 +0300

    fix test

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,9 +1,10 @@
 'use strict';
 
-$(document).ready(function () { 
+$(document).ready(function () { 	
 	let nextItems = $('.js-next-items');
 	nextItems.hide();
     $('.js-next-button').click(function(){
+    	event.preventDefault();
         nextItems.slideDown('fast');
     });
 }); 
